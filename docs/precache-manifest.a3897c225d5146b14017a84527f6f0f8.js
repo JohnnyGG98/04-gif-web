6c320bd081d1783ea3a618649f95ff48a8a5f94b
Johnny Garcia <johnnygar98@hotmail.com>
2020-07-19 21:02:08 -0500

    Agregando Docs para poder utilizar Gifhut pages.

---
+++ docs/precache-manifest.a3897c225d5146b14017a84527f6f0f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c73f1e888886ed09639f86ce94a6ed1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bebe8006269f7c047cc",
+    "url": "/static/css/main.23cb52df.chunk.css"
+  },
+  {
+    "revision": "8d6536eca0cf18dbccb1",
+    "url": "/static/js/2.0f728ee0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0f728ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bebe8006269f7c047cc",
+    "url": "/static/js/main.d002ff3f.chunk.js"
+  },
+  {
+    "revision": "a45fabea812f462b482e",
+    "url": "/static/js/runtime-main.9d5020e6.js"
+  }
+]);
